2804e9b44dd497324a07498517a95aa299a8e691
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-08-16 17:40:00 +0200

    Added round participation model associating players with a round.

---
+++ app/models/round_participation.rb
@@ -0,0 +1,2 @@
+class RoundParticipation < ActiveRecord::Base
+end
---
+++ db/migrate/20080816153810_create_round_participations.rb
@@ -0,0 +1,12 @@
+class CreateRoundParticipations < ActiveRecord::Migration
+  def self.up
+    create_table :round_participations do |t|
+      t.references :player, :round, :null => false
+      t.string :gesture, :null => false
+    end
+  end
+
+  def self.down
+    drop_table :round_participations
+  end
+end
