40776fbabedbe3bd6059d497ac766864cca91d72
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-08-17 01:57:49 +0200

    Added route to rounds.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+
+  map.resources :rounds
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
