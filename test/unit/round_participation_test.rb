5fdc6cc9211f32852c87f44375a1cc3dbe2ebafd
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2008-08-16 17:47:16 +0200

    Added tests and fixture data for round participation.

--- test/unit/round_participation_test.rb
+++ test/unit/round_participation_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class RoundParticipationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_round
+    assert_equal rounds(:one), round_participations(:one_emil).round
+  end
+
+  def test_player
+    assert_equal players(:emil), round_participations(:one_emil).player
+  end
+
+  def test_gesture
+    assert_equal "scissors", round_participations(:one_emil).gesture
   end
 end
